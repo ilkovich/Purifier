43af484450992211a156d0067453fe8156261e84
Peter Csoka <local@local.lo>
2016-01-30 12:40:32 +0000

    Add HTML5 element and attributes from https://github.com/kennberg/php-htmlpurfier-html5

--- config/purifier.php
+++ config/purifier.php
@@ -37,5 +37,56 @@ return [
             "URI.SafeIframeRegexp" => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%",
         ],
     ],
-
+    // HTML5 element and attributes from Alex Kennberg (https://github.com/kennberg/php-htmlpurifier-html5)
+    'addElement' => [
+        // http://developers.whatwg.org/sections.html
+        ['figure', 'Block', 'Optional: (figcaption, Flow) | (Flow, figcaption) | Flow', 'Common'],
+        ['figcaption', 'Inline', 'Flow', 'Common'],
+        ['section', 'Block', 'Flow', 'Common'],
+        ['nav',     'Block', 'Flow', 'Common'],
+        ['article', 'Block', 'Flow', 'Common'],
+        ['aside',   'Block', 'Flow', 'Common'],
+        ['header',  'Block', 'Flow', 'Common'],
+        ['footer',  'Block', 'Flow', 'Common'],
+        // Content model actually excludes several tags, not modelled here
+        ['address', 'Block', 'Flow', 'Common'],
+        ['hgroup', 'Block', 'Required: h1 | h2 | h3 | h4 | h5 | h6', 'Common'],
+        // http://developers.whatwg.org/grouping-content.html
+        ['figure', 'Block', 'Optional: (figcaption, Flow) | (Flow, figcaption) | Flow', 'Common', [ 'contenteditable' => 'Text' ]],
+        ['figcaption', 'Inline', 'Flow', 'Common'],
+        // http://developers.whatwg.org/the-video-element.html#the-video-element
+        ['video', 'Block', 'Optional: (source, Flow) | (Flow, source) | Flow', 'Common', [
+            'src' => 'URI',
+            'type' => 'Text',
+            'width' => 'Length',
+            'height' => 'Length',
+            'poster' => 'URI',
+            'preload' => 'Enum#auto,metadata,none',
+            'controls' => 'Bool',
+        ]],
+        ['source', 'Block', 'Flow', 'Common', [ 'src' => 'URI', 'type' => 'Text']],
+        // http://developers.whatwg.org/text-level-semantics.html
+        ['s',    'Inline', 'Inline', 'Common'],
+        ['var',  'Inline', 'Inline', 'Common'],
+        ['sub',  'Inline', 'Inline', 'Common'],
+        ['sup',  'Inline', 'Inline', 'Common'],
+        ['mark', 'Inline', 'Inline', 'Common'],
+        ['wbr',  'Inline', 'Empty', 'Core'],
+        // http://developers.whatwg.org/edits.html
+        ['ins', 'Block', 'Flow', 'Common', ['cite' => 'URI', 'datetime' => 'CDATA']],
+        ['del', 'Block', 'Flow', 'Common', ['cite' => 'URI', 'datetime' => 'CDATA']],
+    ],
+    'addAttribute' => [
+        // TinyMCE
+        ['img', 'data-mce-src', 'Text'],
+        ['img', 'data-mce-json', 'Text'],
+        // Others
+        ['iframe', 'allowfullscreen', 'Bool'],
+        ['table', 'height', 'Text'],
+        ['td', 'border', 'Text'],
+        ['th', 'border', 'Text'],
+        ['tr', 'width', 'Text'],
+        ['tr', 'height', 'Text'],
+        ['tr', 'border', 'Text'],
+    ]
 ];
